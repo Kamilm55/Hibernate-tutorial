e4fc8e8fec3e86ee7a9e286ea854da9cc47f75cc
Kamilm55 <kamilmmmdov2905@gmail.com>
2023-09-10 07:03:21 +0400

    hibernate tutorial 13th video

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,39 @@
 package org.example;
-
-/**
- * Hello world!
- *
- */
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.BootstrapServiceRegistryBuilder;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+// In very simple terms we can say JPA (Java persistence API) is like an interface and the hibernate is the implementation of the methods of the interface Like how insertion will be down is already defined with the help of hibernate
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        FullName eFName = new FullName();
+        eFName.setFirstName("Emp");
+        eFName.setLastName("Lastname");
+       Employee employee = new Employee();
+       employee.setId(3);
+       employee.setFul_name(eFName);
+       employee.setSalary(1200);
+
+//        System.out.println(employee);
+
+        Configuration config = new Configuration().configure().addAnnotatedClass(Employee.class);
+
+        SessionFactory sessionFactory = config.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+
+        Transaction tx = session.beginTransaction();
+
+
+        session.save(employee);
+        Employee getemployee = session.get(Employee.class,3);
+        tx.commit();
+
+        System.out.println(getemployee);
     }
 }
--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -1,5 +1,47 @@
 package org.example;
 
-public class Employee {
+import jakarta.persistence.*;
 
+@Entity()
+@Table(name = "employees")
+public class Employee {
+    @Id  // every '@Entity' class must declare or inherit at least one '@Id' or '@EmbeddedId' property
+//    @Column(name = "employee_id")
+   private long id;
+   private FullName ful_name ;
+//    @Transient
+    private int salary;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public FullName getFul_name() {
+        return ful_name;
+    }
+
+    public void setFul_name(FullName ful_name) {
+        this.ful_name = ful_name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", ful_name=" + ful_name +
+                ", salary=" + salary +
+                '}';
+    }
 }
